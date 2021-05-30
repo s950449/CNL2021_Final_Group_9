62fd52d4d43497172403a5c403e96131f4eec64c
s950449 <950449@hotmail.com.tw>
2021-05-30 23:31:08 +0800

    Add flask sample

--- server/server.py
+++ server/server.py
@@ -1,6 +1,32 @@
 from mail import mail
-sender = "b07902125@csie.ntu.edu.tw"
-receiver = "b07902125@csie.ntu.edu.tw"
-course_name = "Testing"
-random_token = "random"
-mail(sender,receiver,course_name,random_token)
+from flask import Flask,request, render_template,jsonify
+import requests
+import configparser
+import os
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "test"
+@app.route('/check',methods=['POST','GET'])
+def check():
+    #student = request.values['random_token']
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        src_ip = jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+    else:
+        src_ip = jsonify({'ip': request.environ['HTTP_X_FORWARDED_FOR']}), 200
+    print(src_ip)     
+    return "You are online"   
+if __name__ == '__main__':
+    from sys import argv
+    config = configparser.ConfigParser()
+    config.read('config')
+    course_name = "Testing"
+    random_token = "random"
+    receiver="b07902125@csie.ntu.edu.tw"
+    pid = os.fork()
+    if pid > 0:
+        print("Initialize Web Server")
+        app.run(host='0.0.0.0')
+    else:
+        print("Initialize Mail Server")
+        mail(receiver,course_name,random_token)
