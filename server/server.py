ceb05a6d9904b7aaf033a6f9d842660946edb051
s950449 <950449@hotmail.com.tw>
2021-06-02 16:08:53 +0800

    Update server method

--- server/server.py
+++ server/server.py
@@ -4,8 +4,10 @@ import requests
 import configparser
 import os
 app = Flask(__name__)
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def home():
+    if request.method == 'POST':
+        print(request.values)
     return "test"
 @app.route('/check',methods=['POST','GET'])
 def check():
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     pid = os.fork()
     if pid > 0:
         print("Initialize Web Server")
-        app.run(host='0.0.0.0')
+        app.run(host='0.0.0.0',port=8000)
     else:
         print("Initialize Mail Server")
         mail(receiver,course_name,random_token)
