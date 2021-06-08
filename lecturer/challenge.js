6934efd1341e5e766b98bc11cbaa1a34b019fda5
Patrick8894 <45711086+Patrick8894@users.noreply.github.com>
2021-06-08 20:12:09 +0800

    add challenge.js

---
+++ lecturer/challenge.js
@@ -0,0 +1,31 @@
+import { sendServer } from './server.js';
+var challengeBotton = document.getElementById('challenge');
+challengeBotton.onclick = function () {
+    submit();
+    location.reload();
+};
+
+async function challenge_submit() {
+    let masterToken = document.getElementById('masterToken').value;
+    let courseID = document.getElementById('courseID').value;
+    let type = document.getElementById('type').value;
+    let target = document.getElementById('target').value;
+    let time = document.getElementById('time').value;
+
+    let formData = new FormData();
+
+    formData.append("masterToken", masterToken);
+    formData.append("courseID", courseID);
+    formData.append("type", type);
+    formData.append("target", target);
+    formData.append("time", time);
+
+    let responseMsg = sendServer(formData, "challenge");
+    if (responseMsg.code == 0) {
+        alert("challenge success");
+    }
+    else {
+        alert("challenge failed, please try again");
+    }
+}
+
